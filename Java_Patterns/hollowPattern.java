c8ab5a70160f1bd30957328f961b6353db052009
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-08-17 19:48:06 +0530

    Patterns Advanced Problems

--- Java_Patterns/hollowPattern.java
+++ Java_Patterns/hollowPattern.java
@@ -43,11 +43,64 @@ public class hollowPattern {
         }
     }
 
+    public static void numbersHalfPyramidPattern (int rows) {
+
+        /* Outer Loop -> Rows */
+        for (int i = 1; i <= rows; i++) {
+            
+            /* Inner Loop -> Numbers */
+            for (int j = 1; j <= rows - i + 1; j++) {
+                
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floydsTriangle (int rows) {
+
+        int num = 1;
+        /* Outer loop -> Rows */
+        for(int i = 1; i <= rows; i++) {
+
+            /* Inner loop for number and column */
+            for(int j = 1; j <= i; j ++) {
+
+                System.out.print(num + " ");
+                num ++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zeroOneTrianglePattern (int rows) {
+
+        /* Outer lopp -> Rows */
+        for(int i = 1; i <= rows; i++) {
+
+            /* Inner loop -> Columns */
+            for(int j = 1; j <= i; j++) {
+
+                /* Only print 1 if the position of the Matrix is Even */
+                if ((i + j) % 2 == 0) {
+                    System.err.print("1 ");
+                }
+                else {
+                    System.err.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 
     public static void main(String args[]) {
 
         // hollowRectangle(4, 5);
-        invertedHalfPyramidPattern(4);
+        // invertedHalfPyramidPattern(4);
+        // numbersHalfPyramidPattern(5);
+        // floydsTriangle(5);
+        zeroOneTrianglePattern(5);
 
     }
 
