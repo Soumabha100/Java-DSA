47106bdbed07162186886035a8554c811daf88fb
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-08-17 23:05:58 +0530

    Diamond Pattern

--- Java_Patterns/hollowPattern.java
+++ Java_Patterns/hollowPattern.java
@@ -191,6 +191,43 @@ public class hollowPattern {
         }
     }
 
+    public static void diamond (int rows) {
+
+        /* First half
+        Outer Loop -> Rows */
+        for(int i = 1; i <= rows; i++) {
+
+            /* Spaces */
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            /* Stars */
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        /* Second half
+        Outer Loop -> Rows */
+        for(int i = rows; i >= 1; i--) {
+
+            /* Spaces */
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            /* Stars */
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
 
     public static void main(String args[]) {
 
@@ -201,7 +238,8 @@ public class hollowPattern {
         // zeroOneTrianglePattern(5);
         // butterFly(4);
         // rhombusSolid(5);
-        rhombusHollow(5);
+        // rhombusHollow(5);
+        diamond(4);
 
     }
 
