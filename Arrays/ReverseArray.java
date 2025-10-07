158ff7623329e7e7ab223389fc0a530625d64778
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-10-08 01:08:18 +0530

    Arrays 3

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,28 @@
+public class ReverseArray {
+
+    public static void Reverse (int numbers []) {
+        
+        int first = 0, last = numbers.length - 1;
+
+        while (first < last) {
+            int temp = numbers[first];
+            numbers[first] = numbers[last];
+            numbers[last] = temp;
+
+            first ++;
+            last --;
+        }
+    }
+    
+    public static void main (String args[]) {
+        int numbers [] = {1,2,3,4,5,6,7,8,9,10};
+
+        Reverse(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println( );
+
+    }
+}
