8c577c87c491f68db31c5f231003c6ea153e2f8a
Soumabha Majumder <soumabhamajumder@gmail.com>
2025-10-07 20:19:00 +0530

    Arrays

---
+++ Arrays/Arrays.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Arrays {
+    
+    public static int LinearSearch(int numbers[], int key) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String args[]) {
+
+        int numbers[] = { 2, 4, 5, 8, 12, 14, 19, 21, 29 };
+        int key = 99;
+
+        int index = LinearSearch(numbers, key);
+
+        if (index == -1) {
+            System.out.println("The value does not exists");
+        } else {
+
+            System.out.println("The value " + key + " is found at " + index);
+        }
+
+    }
+}
